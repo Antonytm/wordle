2a3a54f3dea0c747e0485e9972574edb7362c73a
Antonytm <antontishchenko@gmail.com>
2022-01-23 09:23:04 +0200

    Update popular words list

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -11,7 +11,7 @@ export const isWinningWord = (word: string) => {
 
 export const getWordOfDay = () => {
   // January 1, 2022 Game Epoch
-  const epochMs = 1641013200000;
+  const epochMs = 1642914000000;
   const now = Date.now();
   const msInDay = 86400000;
   const index = Math.floor((now - epochMs) / msInDay);
